2d862263e8b586e0d5919a6dfd533c7b760ef4f0
马星俊 <844300634@qq.com>
2023-03-15 11:26:13 +0800

    添加Eureka7002模块并启用服务集群 添加payment模块8002并注册集群

---
+++ cloud-eureka-server7002/src/main/java/com/atguigu/EurekaMain7002.java
@@ -0,0 +1,20 @@
+package com.atguigu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @ClassName EurekaMain7002
+ * @Author maxingjun@xci96716.com
+ * @Since 2023/3/15 10:19
+ * @Description
+ * @Version 1.0
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class,args);
+    }
+}
---
+++ cloud-provider-payment8002/src/main/java/com/atguigu/service/PaymentService.java
@@ -0,0 +1,30 @@
+package com.atguigu.service;
+
+import com.atguigu.dao.PaymentDao;
+import com.atguigu.entity.Payment;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * @ClassName PaymentService
+ * @Author maxingjun@xci96716.com
+ * @Since 2023/3/10 16:55
+ * @Description
+ * @Version 1.0
+ */
+@Service
+public class PaymentService {
+
+    @Resource
+    private PaymentDao paymentDao;
+
+    public int create(Payment payment){
+        return paymentDao.create(payment);
+    };
+
+    public Payment selectById(Long id){
+        return paymentDao.selectById(id);
+    };
+
+}
