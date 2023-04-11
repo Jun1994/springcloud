eda76f27a50ee95114aeb671c890500a3045abf4
马星俊 <844300634@qq.com>
2023-04-11 22:26:11 +0800

    springCloud GateWay模块相关配置

---
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/GateWayMain9527.java
@@ -0,0 +1,18 @@
+package com.atguigu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName GateWayMain9527
+ * @Author maxingjun@xci96716.com
+ * @Since 2023/4/9 12:44
+ * @Description
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+}
