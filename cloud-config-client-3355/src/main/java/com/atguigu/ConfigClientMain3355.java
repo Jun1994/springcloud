55f4473891caf88620935bfa47c6473038829332
马星俊 <844300634@qq.com>
2023-04-13 22:15:45 +0800

    springCloud-config模块 3355客户端微服务 通过3344微服务获取配置内容

---
+++ cloud-config-client-3355/src/main/java/com/atguigu/ConfigClientMain3355.java
@@ -0,0 +1,20 @@
+package com.atguigu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName ConfigClientMain3355
+ * @Author maxingjun@xci96716.com
+ * @Since 2023/4/13 16:05
+ * @Description
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class ConfigClientMain3355 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+
+}
