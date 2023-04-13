9f4406c774e9a862c5e2bc1f471ef1917b168e2f
马星俊 <844300634@qq.com>
2023-04-13 15:08:02 +0800

    springCloud-config模块 3344服务端微服务 通过Config微服务从GitHub上获取配置内容

---
+++ cloud-config-center-3344/src/main/java/com/atguigu/ConfigCenterMain3344.java
@@ -0,0 +1,22 @@
+package com.atguigu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @ClassName ConfigCenterMain3344
+ * @Author maxingjun@xci96716.com
+ * @Since 2023/4/13 11:40
+ * @Description
+ * @Version 1.0
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+
+}
