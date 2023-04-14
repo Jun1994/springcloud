9136af10bc9c1909022f25d4244e3a83a892b9a9
马星俊 <844300634@qq.com>
2023-04-14 16:31:42 +0800

    springCloud-config模块 3366第二个config客户端 同样通过3344微服务获取配置内容

---
+++ cloud-config-client-3366/src/main/java/com/atguigu/ConfigClientMain3366.java
@@ -0,0 +1,20 @@
+package com.atguigu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName ConfigClientMain3366
+ * @Author maxingjun@xci96716.com
+ * @Since 2023/4/14 16:22
+ * @Description
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class ConfigClientMain3366 {
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+
+}
---
+++ cloud-config-client-3366/src/main/java/com/atguigu/controller/ConfigClientController.java
@@ -0,0 +1,31 @@
+package com.atguigu.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName ConfigClientController
+ * @Author maxingjun@xci96716.com
+ * @Since 2023/4/14 16:23
+ * @Description
+ * @Version 1.0
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String configInfo()
+    {
+        return "serverPort: "+serverPort+"\t\n\n configInfo: "+configInfo;
+    }
+
+}
